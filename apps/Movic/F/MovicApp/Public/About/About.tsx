a5e7110d58d85360b597864f35979a3aa90989df
Wima <96892670+WimaDotOne@users.noreply.github.com>
2022-04-13 17:46:21 -0500

    movic

--- apps/Movic/F/MovicApp/Public/About/About.tsx
+++ apps/Movic/F/MovicApp/Public/About/About.tsx
@@ -7,13 +7,13 @@ export function About() {
   return(<>
     <MovicWindow>
       <LimitWidth maxWidth={800}>
-        <AppleNewsHeader1 text1="Movic" text2="If we were a movie" />
+        <AppleNewsHeader1 text1="Movic" text2="movie + comic" />
       </LimitWidth>
       <LimitWidth gray maxWidth={800}>
         <Div height={40} />
-        <StripeIntroText title="What is Movic?" 
-          text1="Do you have a funny story or an interesting plot to tell? Do you think some moments of your life is just like a movie? Do you have an idea of a movie plot? Be a director of your life."
-          text2="Think 'movic' as a movie in the form of a comic strip. You and your friends make your own short movic by taking pictures and add lines of conversations. It's that simple!"
+        <StripeIntroText title="If we were a movie" 
+          text1="Take a few dozens of film stills and add back the lines from the movie, one turns a movie into a movic."
+          text2="Take a few dozens of photos with your friends in it and add what you would say, one turns their life into a movic."
         />
         <Div height={50} />
       </LimitWidth>
