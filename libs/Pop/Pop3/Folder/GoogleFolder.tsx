7e37f1140b69690954b98444e45610e75c8f268f
Wima <96892670+WimaDotOne@users.noreply.github.com>
2022-04-21 01:01:47 -0500

    grid

--- libs/Pop/Pop3/Folder/GoogleFolder.tsx
+++ libs/Pop/Pop3/Folder/GoogleFolder.tsx
@@ -3,26 +3,20 @@ import cl from "./GoogleFolder.module.scss"
 
 interface IGoogleFolderProp {
   text: string,
-  width?: number
   onClick: ()=>void
 }
 export function GoogleFolder({
   text,
-  width,
   onClick
 }: IGoogleFolderProp) {
 
-  width = width || 150
-  let textWidth = width - 40
-  if(textWidth < 0) { textWidth = 100 }
-  const textStyle = { width: textWidth + "px"}
   return(<>
     <div className={cl.folder}>
       <div className={cl.folderIcon}>
-        <SvgIcon name="folder.fill" width={20} color="gray" />
+        <SvgIcon name="folder.fill" width={15} color="#aaa" />
       </div>
       <div className={cl.folderName} >
-        <div className={cl.folderNameInner} style={textStyle}>
+        <div className={cl.folderNameInner} >
           {text}
         </div>
       </div>
