af56f15e53186155525f66b8e41eb431e9da42b4
Wima <96892670+WimaDotOne@users.noreply.github.com>
2022-04-27 10:52:18 -0500

    movic icon

---
+++ pages/apps/Settings/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Settings: NextPage = () => {
+
+  return (<>
+    <Head>
+      <title>Settings</title>
+      <meta name="description" content="Settings" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    Settings
+  </>)
+}
+
+
+
+export default Settings
+
