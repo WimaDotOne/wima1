db940e93b0eb90d49b1f5dbc9726823f1cb66a89
Wima <96892670+WimaDotOne@users.noreply.github.com>
2022-04-27 19:28:32 -0500

    Wima Circle

--- apps/WimaHome/F/WimaCircle/WimaCircle.tsx
+++ apps/WimaHome/F/WimaCircle/WimaCircle.tsx
@@ -18,8 +18,10 @@ export function WimaCircle({
   return(<>
     <div className={cl.goToWimaHome}>
       <div className={cl.wimaCircleBtn} onClick={goToWimaHome}>
-        <SvgIcon name="wimacircle1" width={50} />
-        <div className={cl.text}>Wima Home</div>
+        <div className={cl.wimaCirleBtnInner}>
+          <SvgIcon name="wimacircle1" width={60} />
+          <div className={cl.text}>Wima Home</div>
+        </div>
       </div>
     </div>
   </>)
