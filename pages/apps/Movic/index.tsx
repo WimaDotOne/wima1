8117e21daac4bef9f39f6efaadd46c5748bd62ae
Wima <96892670+WimaDotOne@users.noreply.github.com>
2022-05-01 19:24:51 -0500

    fix bug

--- pages/apps/Movic/index.tsx
+++ pages/apps/Movic/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { AppTurn } from '../../../apps/Movic/F/MovicApp/MovicWindow/MovicWindow'
 import { About } from '../../../apps/Movic/F/MovicApp/Public/About/About'
 import { IsWimaUserLogin } from '../../../apps/Wima/fWima'
+import { useAppleWindow } from '../../../libs/Core/Core2/AppleWindow/fAppleWindow'
 
 const Movic: NextPage = () => {
 
+  const win = useAppleWindow()
+  useEffect(()=>{
+    win?.setViewId(AppTurn.About)
+  })
   return (<>
     <Head>
       <title>Movic</title>
