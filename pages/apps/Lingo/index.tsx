ce70a592df5b0336e66fda837f8a50c09d99b824
Fan <36140830+fan2015zheng@users.noreply.github.com>
2022-06-29 08:49:29 -0500

    Lingo Start

---
+++ pages/apps/Lingo/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function LingoPage() {
+
+
+  return (<>
+    <Head>
+      <title>Lingo</title>
+      <meta name="description" content="Lingo" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  </>)
+}
+
+
