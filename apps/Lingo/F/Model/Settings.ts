a795a792deb782d409ebf3553fc41f178474e18b
Fan <36140830+fan2015zheng@users.noreply.github.com>
2022-08-03 10:52:14 -0500

    unit 8 start

--- apps/Lingo/F/Model/Settings.ts
+++ apps/Lingo/F/Model/Settings.ts
@@ -4,7 +4,7 @@ import { Lang } from "./Lang"
 const Settings = {
   German: [8, 6], // [8,5] means 2 levels with 8 units and 5 units
   French: [],
-  Chinese: [7]
+  Chinese: [8]
 }
 
 export function CalcUnit(lang: string, level: string, lesson: string) {
