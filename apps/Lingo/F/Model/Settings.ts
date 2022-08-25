d346cb24765dfe086bc4d875285fd47a431c2606
Fan <36140830+fan2015zheng@users.noreply.github.com>
2022-08-25 11:00:03 -0500

    spanish 1

--- apps/Lingo/F/Model/Settings.ts
+++ apps/Lingo/F/Model/Settings.ts
@@ -4,7 +4,7 @@ import { Lang } from "./Lang"
 const Settings = {
   German: [8, 6], // [8,5] means 2 levels with 8 units and 5 units
   French: [],
-  Spanish: [],
+  Spanish: [1],
   Chinese: [8, 5]
 }
 
