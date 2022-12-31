7b9f51204edb8b095d2a7238d14bd7f47cd595aa
Wima <96892670+WimaDotOne@users.noreply.github.com>
2022-12-30 18:26:52 -0600

    Quiz Player head title

--- pages/apps/Quiz/ChapterPlay.tsx
+++ pages/apps/Quiz/ChapterPlay.tsx
@@ -1,7 +1,8 @@
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { ChapterQuizPlayer } from '../../../apps/Quiz/F/QuizApp/QuizPlayer/ChapterQuizPlayer/ChapterQuizPlayer'
 
-export default function MovicPlay() {
+export default function ChapterPlay() {
 
   const router = useRouter()
   const chapterId = router.query.chapterId as string
@@ -10,6 +11,11 @@ export default function MovicPlay() {
     router.push("/")
   }
   return (<>
+  <Head>
+    <title>Quiz</title>
+    <meta name="description" content="Quiz" />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
   <ChapterQuizPlayer chapterId={chapterId} onBack={back}/>
   </>)
 }
