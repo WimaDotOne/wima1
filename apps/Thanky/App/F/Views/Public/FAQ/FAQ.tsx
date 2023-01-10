ad652657c60217ac4b3145cd9aa7140147ee6ae6
Wima <96892670+WimaDotOne@users.noreply.github.com>
2023-01-09 19:00:14 -0600

    FAQ content

--- apps/Thanky/App/F/Views/Public/FAQ/FAQ.tsx
+++ apps/Thanky/App/F/Views/Public/FAQ/FAQ.tsx
@@ -1,4 +1,5 @@
-import { Div } from "../../../../../../../libs/Core/Core2/fCore2"
+import { Div, LimitWidth } from "../../../../../../../libs/Core/Core2/fCore2"
+import { Afaq } from "../../../../../Lib/Afaq/Afaq"
 import { FourItemNavBar } from "../../../../../Lib/FourItemNavBar/FourItemNavBar"
 import { HeroHeader } from "../../../../../Lib/HeroHeader/HeroHeader"
 import { IceCreamWoodButton } from "../../../../../Lib/IceCreamWoodButton/IceCreamWoodButton"
@@ -25,10 +26,35 @@ export function FAQ({
       smallWords="If you can't find an answer that you're looking for, feel free to email us."
     />
     <div className={cl.buttonsSpace}>
-      <IceCreamWoodButton text="Contact us" 
+      <IceCreamWoodButton text="Contact" 
         onClick={openContactUs}/>
     </div>
     <Div height={20} />
   </div>
+  <Div height={20} />
+  <LimitWidth maxWidth={800}>
+    <Afaq question="Who can use Thanky to accept tips?" 
+      answerParagrahs={[
+        "The only requirement is that your service is associated with an establishemnt with a physical location and you serve people face to face.",
+        "You can be a waitress at a restaurant. You can be a cashier at a grocery store. You can be a voluteering tutor at a university."
+      ]} />
+    <Afaq question="How employees get tips?" 
+      answerParagrahs={[
+        "Your tips will go to your local bank account. There’s a one-time process of connecting your Stripe account. Creating a Stripe account is free."
+      ]}
+    />
+    <Afaq question="How can customers tip?"
+      answerParagrahs={[
+        "We support all major credit and debit cards"
+      ]}
+    />
+    <Afaq question="Is there a fee to use Thanky?"
+      answerParagrahs={[
+        "We do not charge a monthly fee. All features are free for everyone.",
+        "When a customer tips a server using credit card, a transaction fee of 2.9% and 50 cents will be taken away from the tip."
+      ]}
+    />
+  </LimitWidth>
+  <Div height={100} />
   </>)
 } 
