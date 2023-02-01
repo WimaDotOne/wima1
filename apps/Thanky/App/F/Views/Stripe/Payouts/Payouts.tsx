4ebc81ec58d1a495b0c9c80468582cd47e0c29b4
Wima <96892670+WimaDotOne@users.noreply.github.com>
2023-01-31 20:16:17 -0600

    explain stripe before Q&A

--- apps/Thanky/App/F/Views/Stripe/Payouts/Payouts.tsx
+++ apps/Thanky/App/F/Views/Stripe/Payouts/Payouts.tsx
@@ -5,6 +5,8 @@ import cl from "./Payouts.module.scss"
 import { Div, SvgIcon } from "../../../../../../../libs/Core/Core2/fCore2"
 import { Post2, StripeButton, useShield } from "../../../../../../../libs/Core/Core1/fCore1"
 import { useRouter } from "next/router"
+import { TwoStickyNotes } from "../../../../../Lib/TwoStickyNotes/TwoStickyNotes"
+import { QueAns } from "../../../../../Lib/QueAns/QueAns"
 
 interface IPayoutsProp {
 
@@ -28,6 +30,36 @@ export function Payouts({
   return(<>
   <ThankyWindow selectItemId={ThankyMenuTurn.Payouts}>
     <div className={cl.pageTitle}>Payouts</div>
+    <div className={cl.explainSpace}>
+      <div className={cl.explainHeader}>
+        <span className={cl.explainWord}>explain</span> <span className={cl.stripeWord}>Stripe</span></div>
+      <div className={cl.explainOneliner}>
+        Stripe is the name of a company that handles online payment.
+      </div>
+      <Div height={40} />
+      <TwoStickyNotes title1="" 
+        text1="In order to receive tips online, you need to create a Stripe account." 
+        text2="Stripe treats payment receiver as a business that provides a service for a fee."
+      />
+    </div>
+    <QueAns />
+
+    <CoffeeWindowViewCard>
+      <div>Explain Stripe</div>
+      <ul>
+        <li>Country</li>
+        <li>Type of business</li>
+        <li>Email</li>
+        <li>Phone number</li>
+        <li>Legal name</li>
+        <li>Date of birth</li>
+        <li>Home address</li>
+        <li>Last 4 digits of Social Security Number</li>
+        <li>Industry</li>
+        <li>Website</li>
+        <li>Bank account or Debit card</li>
+      </ul>
+    </CoffeeWindowViewCard>
     <CoffeeWindowViewCard>
       <div className={cl.instructionFrame}>
         <SvgIcon name="lightening" width={32} color="#333"/>
@@ -45,6 +77,7 @@ export function Payouts({
         />
       </div>
     </CoffeeWindowViewCard>
+
   </ThankyWindow>
   </>)
 }
---
+++ apps/Thanky/Lib/QueAns/QueAns.tsx
@@ -0,0 +1,11 @@
+import cl from "./QueAns.module.scss"
+
+interface IQueAnsProp {
+
+}
+
+export function QueAns({}: IQueAnsProp) {
+  return(<>
+  Q & As
+  </>)
+}
