2eb2c9905395a0826c5bb57f1364ecc20cff36e5
Wima <96892670+WimaDotOne@users.noreply.github.com>
2023-03-19 15:24:02 -0500

    add more route

--- admin/Lingo/B/Router/LingoAdminRouter.js
+++ admin/Lingo/B/Router/LingoAdminRouter.js
@@ -1,8 +1,12 @@
 import express from "express"
 import { iImageMulter } from "../../../../libs/Core/Core1/bCore1.js"
 import { iAdminAuth } from "../../../WimaAdmin/B/Router/iAdminAuth.js"
+import { iChangeImageNames } from "./iChangeImageNames.js"
 import { iCreateUnit } from "./iCreateUnit.js"
+import { iDeleteUnitImages } from "./iDeleteUnitImages.js"
+import { iLoadImageFolder } from "./iLoadImageFolder.js"
 import { iLoadLanguageUnits } from "./iLoadLanguageUnits.js"
+import { iLoadUnitScript } from "./iLoadUnitScript.js"
 import { iNewTempFolder } from "./ImageUpload/iNewTempFolder.js"
 import { iUploadImages } from "./ImageUpload/iUploadImages.js"
 
@@ -14,6 +18,13 @@ LingoAdminRouter.post("/UploadImages", iAdminAuth, iNewTempFolder, iImageMulter,
 
 LingoAdminRouter.post('/CreateUnit', iAdminAuth, iCreateUnit)
 LingoAdminRouter.get('/LoadLanguageUnits', iAdminAuth, iLoadLanguageUnits)
+
+LingoAdminRouter.get('/LoadImageFolder', iAdminAuth, iLoadImageFolder)
+LingoAdminRouter.post("/ChangeImageNames", iAdminAuth, iChangeImageNames)
+LingoAdminRouter.post("/DeleteUnitImages", iAdminAuth, iDeleteUnitImages)
+LingoAdminRouter.get('/LoadUnitScript', iAdminAuth, iLoadUnitScript)
+
+
 
 export {
   LingoAdminRouter
